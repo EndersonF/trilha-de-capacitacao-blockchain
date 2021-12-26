b25f0a0be3b20d3b4034324f2d239db0af5dc10f
Enderson Frazão <enderson.frazao7@gmail.com>
2021-12-26 12:32:18 -0400

    ex-01-alura

---
+++ JS/Arrays/nota-escolares-arrays.js
@@ -0,0 +1,17 @@
+//ALGORITMO DE SISTEMAS DE NOTA ESCOLARES
+
+// let nota1 = 7.5;
+// let nota2 = 6.0
+// let nota3 = 8.0
+// let nota4 = 5.5
+
+// let media = (nota1+nota2+nota3+nota4) /4
+
+// console.log(media)
+
+
+//USANDO ARRAYS//
+const notas = [7.5, 6.0, 8.0, 5.5];
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
