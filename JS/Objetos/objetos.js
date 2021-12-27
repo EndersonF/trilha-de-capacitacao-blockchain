b16c353d56af83077bcff5e3a847dcddb467062d
Enderson Frazão <enderson.frazao7@gmail.com>
2021-12-27 13:02:44 -0400

    desafios-alura

---
+++ JS/Objetos/objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['73851669037', '74808536021', '78120650069']
+
+// const cliente = ['nome','André','idade',36]
+
+const cliente = {
+    nome: "André",
+    idade: 36,
+    cpf: '15534675071',
+    email: 'andre@email.com'
+}
